e309b106c294260c2cd99337f8d54a3bb3f70266
sidsharma27 <sidsharma27@yahoo.com>
2017-01-10 21:59:25 -0800

    Send SMS using Nexmo Dot Net Library

---
+++ SendSMSDotNET/SendSMSDotNET/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nexmo.Api;
+namespace SendSMSDotNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the text you want to send: ");
+            string text = Console.ReadLine();
+            var results = SMS.Send(new SMS.SMSRequest
+            {
+                from = "",
+                to = "",
+                text = text
+            });
+        }
+    }
+}
